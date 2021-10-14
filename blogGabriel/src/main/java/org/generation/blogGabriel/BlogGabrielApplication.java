13898fc5f16635066e5ce760de6b1bf71597a34e
Gabrielgjs <gjsgabriel83@gmail.com>
2021-10-14 17:37:46 -0300

    BlogGabrielInicio

---
+++ blogGabriel/src/main/java/org/generation/blogGabriel/BlogGabrielApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogGabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogGabrielApplication.class, args);
+	}
+
+}
---
+++ blogGabriel/src/test/java/org/generation/blogGabriel/BlogGabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogGabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogGabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
