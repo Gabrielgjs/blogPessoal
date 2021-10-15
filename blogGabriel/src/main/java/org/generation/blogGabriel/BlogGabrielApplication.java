fd74b7071076ce260e7c760e5c5bb262e260c1ca
Gabrielgjs <gjsgabriel83@gmail.com>
2021-10-14 22:06:11 -0300

    Blog pessoal Deploy

--- blogGabriel/src/main/java/org/generation/blogGabriel/BlogGabrielApplication.java
+++ blogGabriel/src/main/java/org/generation/blogGabriel/BlogGabrielApplication.java
@@ -2,10 +2,21 @@ package org.generation.blogGabriel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BlogGabrielApplication {
 
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogGabrielApplication.class, args);
 	}
---
+++ blogGabriel/src/main/java/org/generation/blogGabriel/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package org.generation.blogGabriel.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.blogGabriel.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public Usuario findByNome(String nome);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+}
